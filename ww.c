aa655902dfd2d757f8cf3549276d614fd183269c
Saipranav Kalapala <sk2048@ilab4.cs.rutgers.edu>
2021-03-06 17:12:16 -0500

    testing functions

--- ww.c
+++ ww.c
@@ -15,11 +15,50 @@ int isdir(char *name) {
 		return 0;
 	}
 
-	if (S_ISDIR()data.st_mode) {return 1;}
+	if (S_ISDIR(data.st_mode)) {return 1;}
 
 	return 0;
 }
 
-int main() {
+int r_dir(char* path) {
 
+	struct dirent *de;
+	
+	DIR *dr = opendir(path);
+
+	if (dr == 0) {
+		perror(path);
+		return 0;
+	}
+
+	while ((de = readdir(dr)) != NULL) {
+		printf("Name: %s\n", de->d_name);
+		printf("Type: %d\n", de->d_type);
+		//printf("  \n");
+
+		if (!strcmp(de->d_name, ".") || !strcmp(de->d_name, "..")) {
+			//printf("waewgtfsdg\n");
+			continue;
+		}
+
+		if (de->d_type == 4) {
+			//printf("true\n");
+			char* new_path = malloc(strlen(de->d_name) + strlen(path) + 2);
+			new_path = strcpy(new_path, path);
+			strcat(new_path, "/");
+			strcat(new_path, de->d_name);
+			printf("path name: %s\n", new_path);
+		} else {
+			//printf("false\n");
+		}
+	}
+
+	closedir(dr);
+	return 1;
+
+}
+
+int main() {
+	printf("%d\n", isdir("test"));
+	r_dir("test");
 }
